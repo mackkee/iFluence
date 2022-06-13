495e5cbc1ecf0b31f54c1380825aa2c742a4807c
Mackke <marcus.karlsson@howwe.io>
2022-06-13 19:20:48 +0200

    Initial commit

---
+++ src/main/kotlin/com/md/iFluence/IFluenceApplication.kt
@@ -0,0 +1,11 @@
+package com.md.iFluence
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IFluenceApplication
+
+fun main(args: Array<String>) {
+	runApplication<IFluenceApplication>(*args)
+}
---
+++ src/test/kotlin/com/md/iFluence/IFluenceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.md.iFluence
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class IFluenceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
